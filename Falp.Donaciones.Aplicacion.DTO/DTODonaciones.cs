735ec18dbe86a5713db44379c5fb0a3e7e1f231e
Esteban <slean1@gmail.com>
2024-11-25 23:55:06 -0300

    Add files via upload
    capa aplicación completa

---
+++ Falp.Donaciones.Aplicacion.DTO/DTODonaciones.cs
@@ -0,0 +1,24 @@
+﻿namespace Falp.Donaciones.Aplicacion.DTO
+{
+    public class DTODonaciones
+    {
+        public string Mandato { get; set; }
+        public string Rut { get; set; }
+        public string NroDocto { get; set; }
+        public string TipoDonante { get; set; }
+        public string FormaPago { get; set; }
+        public string MontoCargado { get; set; }
+        public string MesCobro { get; set; }
+        public string CBeneficio { get; set; }
+        public string FechaVcmto { get; set; }
+        public string IdCargaPac { get; set; }
+        public string FechaCarga { get; set; }
+        public string FechaCaidaPago { get; set; }
+        public string PeriodoCargo { get; set; }
+        public string PeriodoCargoDos { get; set; }
+        public string BancoCodigo { get; set; }
+        public string IdParticipacion { get; set; }
+        public string ProenCorrelativo { get; set; }
+        public string CodAutorizacion { get; set; }
+    }
+}
---
+++ Falp.Donaciones.Aplicacion.Main/DonacionApplication.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Falp.Donaciones.Aplicacion.DTO;
+using Falp.Donaciones.Aplicacion.Interface;
+using Falp.Donaciones.Transversal.Comunes;
+using Falp.Donaciones.Dominio.Entidad;
+using Falp.Donaciones.Dominio.Interface;
+using AutoMapper;
+
+namespace Falp.Donaciones.Aplicacion.Main
+{
+    class DonacionApplication: IDonacionesAplicacion
+    {
+        private readonly IDonacionesDominio _donacionesDominio;
+        private readonly IMapper _mapper;
+
+        public DonacionApplication (IDonacionesDominio donacionDominio, IMapper mapper)
+        {
+            _donacionesDominio = donacionDominio;
+            _mapper = mapper;
+        }
+
+        #region Métodos
+        public Response<bool> Insertar(DTODonaciones dtoDonacion)
+        {
+            var response = new Response<bool>();
+            try
+            {
+                var donacion = _mapper.Map<Donacion>(dtoDonacion);
+                response.Data = _donacionesDominio.Insertar(donacion);
+                if (response.Data)
+                {
+                    response.IsSucess = true;
+                    response.Message = "Donación registrada";
+                }
+            }
+            catch (Exception e)
+            {
+                response.Message = e.Message;
+            }
+            return response;
+        }
+        #endregion
+    }
+}
