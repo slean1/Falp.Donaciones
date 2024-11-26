c21d392d309786aedbf09851cb42f5c8a72ccec8
Esteban <slean1@gmail.com>
2024-11-25 23:59:31 -0300

    Add files via upload
    capa servicio y solución

---
+++ Falp.Donaciones.Servicio.WebApi/Controllers/RecaudacionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Falp.Donaciones.Aplicacion.DTO;
+using Falp.Donaciones.Aplicacion.Interface;
+
+namespace Falp.Donaciones.Servicio.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RecaudacionController : Controller
+    {
+        private readonly IDonacionesAplicacion _donacionesAplicacion;
+
+        public RecaudacionController(IDonacionesAplicacion donacionesAplicacion)
+        {
+            _donacionesAplicacion = donacionesAplicacion;
+        }
+
+        #region Métodos
+        [HttpPost]
+        public IActionResult Insertar ([FromBody] DTODonaciones dtoDonaciones)
+        {
+            if (dtoDonaciones == null)
+                return BadRequest();
+
+            var response = _donacionesAplicacion.Insertar(dtoDonaciones);
+
+            if (response.IsSucess)
+                return Ok(response);
+            else
+                return BadRequest(response.Message);
+        }
+        #endregion
+    }
+}
