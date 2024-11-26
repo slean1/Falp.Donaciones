82d4d4c5d5e1586ffeef1651557ae77a0f734b65
Esteban <slean1@gmail.com>
2024-11-25 23:56:14 -0300

    Add files via upload
    capa dominio

---
+++ Falp.Donaciones.Dominio.Core/DonacionDominio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Falp.Donaciones.Dominio.Entidad;
+using Falp.Donaciones.Dominio.Interface;
+using Falp.Donaciones.Infraestructura.Interface;
+
+namespace Falp.Donaciones.Dominio.Core
+{
+    public class DonacionDominio : IDonacionesDominio
+    {
+        private readonly IDonacionesRepository _donacionesRepository;
+
+        public DonacionDominio(IDonacionesRepository donacionesRepository)
+        {
+            _donacionesRepository = donacionesRepository;
+        }
+
+        #region Métodos
+        public bool Insertar (Donacion donacion)
+        {
+            return _donacionesRepository.Insertar(donacion);
+        }
+        #endregion
+    }
+}
---
+++ Falp.Donaciones.Dominio.Interface/IDonacionesDominio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Falp.Donaciones.Dominio.Entidad;
+
+namespace Falp.Donaciones.Dominio.Interface
+{
+    public interface IDonacionesDominio
+    {
+        #region Métodos
+        bool Insertar(Donacion Donacion);
+        #endregion
+    }
+}
