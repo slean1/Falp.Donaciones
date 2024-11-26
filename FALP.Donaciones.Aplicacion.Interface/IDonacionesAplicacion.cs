0b17b549ca393717d18ca0ea2fdc8c9afbb6e701
Esteban <slean1@gmail.com>
2024-11-25 23:54:06 -0300

    Add files via upload
    interface - aplicación

---
+++ FALP.Donaciones.Aplicacion.Interface/IDonacionesAplicacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Falp.Donaciones.Aplicacion.DTO;
+using Falp.Donaciones.Transversal.Comunes;
+
+namespace Falp.Donaciones.Aplicacion.Interface
+{
+    public interface IDonacionesAplicacion
+    {
+        #region Métodos
+        Response<bool> Insertar(DTODonaciones DtoDonaciones);
+        #endregion
+    }
+}
